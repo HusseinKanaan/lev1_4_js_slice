3ae210adce9f0ad85f5fcc8fde9ba3e95373379b
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-08-02 13:41:23 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+//Aufgabe 1
+const A = 'Susi is going to codingschool';
+let var1 = A.slice(0,5)
+document.write(var1 + "<br>")
+
+//Aufgabe 2
+let var2 = A.slice(5,8)
+document.write(var2 + "<br>")
+
+//Aufgabe 3
+let var3 =A.slice(5,16,)
+let var32 =A.slice(-6)
+ //document.write(var3 + A.slice(-6) + "<br>") 
+//oder
+document.write(var3 + var32 + "<br>")
+
+
+//Aufgabe 4
+let var4 =A.slice(0,7)
+let var43 = A.slice(-6)
+console.log(`${var4} ${var43}`)
